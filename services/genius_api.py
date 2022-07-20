52e28d469ed0028879a0609cf005778348a75b5e
Luciano Feder <lucianoo.feder@gmail.com>
2022-07-20 19:56:54 -0300

    feat: created integration with genius api

---
+++ services/genius_api.py
@@ -0,0 +1,29 @@
+from typing import Any, Dict
+
+import requests
+from resources.configs import get_config
+
+settings = get_config()
+
+
+class Genius:
+    """Class that is responsible to communicate with Genius Api
+    """
+    __token = settings.GENIUS_ACCESS_TOKEN
+    __base_url = settings.GENIUS_BASE_URL
+
+    def search(self, data_to_search: str) -> Dict[str, Any]:
+        """Makes a request to the endpoint '/search' of the api
+
+        Args:
+            data_to_search (str): A string containing what to search
+
+        Returns:
+           Dict[str, Any]:  Response containing what was found
+        """
+        response = requests.get(
+            f'{self.__base_url}/search',
+            {'q': data_to_search},
+            headers=f'Bearer {self.__token}'
+        )
+        return response.json()
