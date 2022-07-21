6eecca2e301638f70b3a9e5b3a917bed557a948f
Luciano Feder <lucianoo.feder@gmail.com>
2022-07-20 22:53:46 -0300

    feat: inserted transaction model tablename on class.

--- app/models/transaction.py
+++ app/models/transaction.py
@@ -6,3 +6,5 @@ from pydantic import BaseModel, Field
 class TransactionModel(BaseModel):
     id: str = Field(default_factory=lambda: str(uuid4()))
     artist: str
+
+    __tablename__ = 'transaction'
